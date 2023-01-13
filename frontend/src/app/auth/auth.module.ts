6ffe5ab86911f5b6627b0f27f486fca51d0f1db8
Ramu Narasinga <ramu@binlo.com.hk>
2023-01-13 16:25:40 +0000

    added auth lazy loading module

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
