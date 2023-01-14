5d661929c2abec6bbfa30bb7708cc1a6738bdcd8
Ramu Narasinga <ramu@binlo.com.hk>
2023-01-14 22:56:06 +0000

    added complete ui design changes and polishing

--- frontend/src/app/modules/email/edit/edit.component.ts
+++ frontend/src/app/modules/email/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Entity, Status } from '../../shared/entity-list/entity-list.component';
 
 @Component({
@@ -6,11 +7,46 @@ import { Entity, Status } from '../../shared/entity-list/entity-list.component';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
   email: Entity = {
     id: 1,
     title: 'Email One',
     description: 'Sample Description',
     status: Status.DRAFT
   }
+
+  isEditorMode = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams
+      .subscribe(params => {
+        console.log(params);
+        this.isEditorMode = JSON.parse(params['isEditorMode'].toLowerCase());
+      }
+    );
+  }
+
+  handleEmailEditBackNav() {
+
+    let emailBackNavToEdit = '/dashboard/email/edit/'+this.email.id;
+    let emailBackNavToList = '/dashboard/email/';
+
+    if (this.isEditorMode) {
+      this.router.navigate(
+        [emailBackNavToEdit],
+        {
+          queryParams: {
+            'isEditorMode': false
+          }
+        }
+      )
+    } else {
+      this.router.navigate([emailBackNavToList])
+    }
+   }
 }
