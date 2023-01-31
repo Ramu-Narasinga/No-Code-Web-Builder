68c5cd9e8c36a7643f16c2332b40ef27a999dc65
Ramu Narasinga <ramu@binlo.com.hk>
2023-01-31 22:16:17 +0000

    added env change to point to production be

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  serverUrl: 'http://134.122.103.117:8080',
+  serverUrl: 'https://warm-bastion-40096.herokuapp.com',
   websiteRendererScriptSrc: `https://ncwb-renderer.s3.eu-west-2.amazonaws.com/index.min.js`,
   ncwbRecorderScript: 'https://ncwb-recorder.s3.eu-west-2.amazonaws.com/index.min.js',
   ncwbRecorderCss: 'https://ncwb-recorder.s3.eu-west-2.amazonaws.com/index.css'
