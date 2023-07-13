6245d288010e808606e5f2220ad74d157e533049
Ramu Narasinga <ramu@binlo.com.hk>
2023-07-13 07:12:50 +0100

    updated env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  serverUrl: 'https://warm-bastion-40096.herokuapp.com',
+  serverUrl: 'https://ncwb-backend.onrender.com',
   websiteRendererScriptSrc: `https://ncwb-renderer.s3.eu-west-2.amazonaws.com/index.min.js`,
   ncwbRecorderScript: 'https://ncwb-recorder.s3.eu-west-2.amazonaws.com/index.min.js',
   ncwbRecorderCss: 'https://ncwb-recorder.s3.eu-west-2.amazonaws.com/index.css'
