be4948eccbeef9a04716fb10ba70ffe078f3e6a1
Ramu Narasinga <ramu@binlo.com.hk>
2023-07-13 07:38:45 +0100

    removed migrations

--- backend/prisma/migrations/20221230174948_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `emailMetaId` on the `Email` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Email` DROP COLUMN `emailMetaId`;
--- backend/prisma/migrations/20230105003613_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `activityEvents` to the `VisitorActivity` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `VisitorActivity` ADD COLUMN `activityEvents` VARCHAR(191) NOT NULL;
